b1f120001ce8be0eb608de518194c050dfc5b1ba
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-05-20 18:10:34 +0100

    Address SonarQube Cloud issue

--- docker/scripts/update-config.sh
+++ docker/scripts/update-config.sh
@@ -2,7 +2,7 @@
 # This script MUST be called from ${containerWorkspace Folder}.
 set -e
 
-CAP_XML_RDS_CONNECTION_STRING=postgres://cx:cx@capxmldb/cx 
+CAP_XML_RDS_CONNECTION_STRING=${CAP_XML_DB_CONNECTION_STRING}
 JQ_CAP_XML_RDS_CONNECTION_STRING=$CAP_XML_RDS_CONNECTION_STRING jq '.aws.rdsConnectionString=$ENV.JQ_CAP_XML_RDS_CONNECTION_STRING' config/config.json > config/tmp-config.json
 mv config/tmp-config.json config/config.json
 echo Updated config/config.json - \$.aws.rdsConnectionString \($CAP_XML_RDS_CONNECTION_STRING\) allows connectivity to the provisioned local Postgres database
